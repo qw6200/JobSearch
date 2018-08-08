655b7d10f9875c7934e42d5838f77e3eaba2b117
Jacob Lee <wlee33@u.rochester.edu>
2018-08-07 23:59:40 -0400

    Set up Spotify API authentication

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SpotifyWebApi from 'spotify-web-api-js';
+const spotifyApi = new SpotifyWebApi();
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		const params = this.getHashParams();
+		const token = params.access_token;
+		if (token) {
+			spotifyApi.setAccessToken(token);
+		}
+		this.state = {
+			loggedIn: token ? true : false,
+			nowPlaying: { name: 'Not Checked', albumArt: '' }
+		}
+	}
+	getHashParams() {
+		var hashParams = {};
+		var e, r = /([^&;=]+)=?([^&;]*)/g,
+			q = window.location.hash.substring(1);
+		e = r.exec(q)
+		while (e) {
+			hashParams[e[1]] = decodeURIComponent(e[2]);
+			e = r.exec(q);
+		}
+		return hashParams;
+	}
+	getNowPlaying() {
+		spotifyApi.getMyCurrentPlaybackState()
+			.then((response) => {
+				this.setState({
+					nowPlaying: {
+						name: response.item.name,
+						albumArt: response.item.album.images[0].url
+					}
+				});
+			})
+	}
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					<a href='http://localhost:8888'> Login to Spotify </a>
+					<div>
+						Now Playing: {this.state.nowPlaying.name}
+					</div>
+					<div>
+						<img src={this.state.nowPlaying.albumArt} alt='album' style={{ height: 150 }} />
+					</div>
+					{this.state.loggedIn &&
+						<button onClick={() => this.getNowPlaying()}>
+							Check Now Playing
+            </button>
+					}
+				</p>
+			</div>
+		);
+	}
 }
 
 export default App;
